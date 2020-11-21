7b7e7f2f8430039be0efd760cc0ac7d3f114fa40
Ubuntu <ubuntu@ip-172-31-36-221.eu-west-1.compute.internal>
2020-11-21 22:54:08 +0000

    tests added

--- scripts/ssh_pytest.sh
+++ scripts/ssh_pytest.sh
@@ -1,5 +1,4 @@
-
-git clone --branch automation https://github.com/Scottynic112/flaskAppProject.git
+git clone --branch dev https://github.com/Scottynic112/flaskAppProject.git
 cd flaskAppProject
 sudo -E DATABASE_URI=${DATABASE_URI} -E TEST_DATABASE_URI=${TEST_DATABASE_URI} -E SECRET_KEY=${SECRET_KEY} docker-compose up -d
 sudo docker exec frontend bash -c "pytest tests/ --cov application"
