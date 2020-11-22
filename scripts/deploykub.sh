cbf86caf2a5e81f879a7c66f62b5d1d5f50b3345
Ubuntu <ubuntu@ip-172-31-36-221.eu-west-1.compute.internal>
2020-11-22 18:30:18 +0000

    tests2

--- scripts/deploykub.sh
+++ scripts/deploykub.sh
@@ -1,6 +1,6 @@
 cd kubernetesProject/
-kubectl apply -f namespace.yaml
-kubectl apply -f secret.yaml
-kubectl apply -f nginxconfig.yaml
-kubectl apply -f lb.yaml -f nginxdeploy.yaml -f frontenddeploy.yaml -f frontendcip.yaml -f backendcip.yaml -f backenddeploy.yaml
-kubectl describe service lb -n flaskapp
+sudo kubectl apply -f namespace.yaml
+sudo kubectl apply -f secret.yaml
+sudo kubectl apply -f nginxconfig.yaml
+sudo kubectl apply -f lb.yaml -f nginxdeploy.yaml -f frontenddeploy.yaml -f frontendcip.yaml -f backendcip.yaml -f backenddeploy.yaml
+sudo kubectl get svc -n flaskapp
