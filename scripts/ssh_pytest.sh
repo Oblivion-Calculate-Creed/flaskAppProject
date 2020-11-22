67a54301901ee85199b748f89c7dc9575757fdea
Scottynic112 <71728095+Scottynic112@users.noreply.github.com>
2020-11-22 01:01:07 +0000

    Update ssh_pytest.sh

--- scripts/ssh_pytest.sh
+++ scripts/ssh_pytest.sh
@@ -5,4 +5,4 @@ sudo -E DATABASE_URI=${DATABASE_URI} -E TEST_DATABASE_URI=${TEST_DATABASE_URI} -
 sudo docker exec frontend bash -c "pytest tests/ --cov application"
 sudo docker exec backend bash -c "pytest tests/ --cov application"
 sudo docker-compose down
-
+rm -rf flaskAppProject
