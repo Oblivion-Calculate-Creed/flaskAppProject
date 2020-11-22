1257af61c057373341e76466c7e8358c3ece7068
Scottynic112 <71728095+Scottynic112@users.noreply.github.com>
2020-11-22 00:07:38 +0000

    Update ssh_pytest.sh

--- scripts/ssh_pytest.sh
+++ scripts/ssh_pytest.sh
@@ -1,6 +1,8 @@
+sudo rm -r flaskAppProject
 git clone --branch dev https://github.com/Scottynic112/flaskAppProject.git
 cd flaskAppProject
 sudo -E DATABASE_URI=${DATABASE_URI} -E TEST_DATABASE_URI=${TEST_DATABASE_URI} -E SECRET_KEY=${SECRET_KEY} sudo docker-compose up -d
 sudo docker exec frontend bash -c "pytest tests/ --cov application"
 sudo docker exec backend bash -c "pytest tests/ --cov application"
 sudo docker-compose down
+rm -rf flaskAppProject
