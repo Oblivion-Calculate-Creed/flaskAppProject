7d6cbd2d9d42e5eaa50564a91deb0adc38fc0c0d
Ubuntu <ubuntu@ip-172-31-36-221.eu-west-1.compute.internal>
2020-11-22 21:29:45 +0000

    remote test

---
+++ scripts/remote_pytest.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ssh -i /home/jenkins/.ssh/id_rsa ubuntu@52.50.198.229 << EOF
+
+sudo rm -r CNE_PracticalProject
+
+git clone https://github.com/Scottynic112/flaskAppProject.git
+
+cd flaskAppProject/
+
+docker-compose up -d --build
+
+docker exec backend bash -c "pytest tests/ --cov application" >> backend_test.txt
+docker exec frontend bash -c "pytest tests/ --cov application" >> frontend_test.txt
+
+docker-compose down
+
+EOF
