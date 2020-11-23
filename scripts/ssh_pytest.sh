452a73b587cb1731a3c75982fbee2b612caa36c9
Ubuntu <ubuntu@ip-172-31-36-221.eu-west-1.compute.internal>
2020-11-23 00:26:51 +0000

    tests

--- scripts/ssh_pytest.sh
+++ scripts/ssh_pytest.sh
@@ -6,7 +6,7 @@ cd /home/ubuntu/flaskAppProject
 
 sudo -E DATABASE_URI=${DATABASE_URI} -E TEST_DATABASE_URI=${TEST_DATABASE_URI} -E SECRET_KEY=${SECRET_KEY} 
 
-docker-compose up -d --build
+sudo docker-compose up -d --build
 
 sudo docker exec frontend bash -c "pytest tests/ --cov application"
 sudo docker exec backend bash -c "pytest tests/ --cov application"
